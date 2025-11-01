574c7691e12038bf402bb5147c42c44dfde428c3
joseph <josephyoongmhk@gmail.com>
2025-11-01 13:10:52 +0000

    Refactor PC_plus4 module input/output syntax

--- PC_plus4.v
+++ PC_plus4.v
@@ -1,10 +1,9 @@
 // PC_plus4
 // PC Plus 4
 
-module PC_plus4 #(
-) (
-    input [31:0] I;
-    output [31:0] O;
+module PC_plus4 (
+    input [31:0] I,
+    output [31:0] O
 );
 
 assign O = I + 4;
