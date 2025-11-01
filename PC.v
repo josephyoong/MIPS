7c6670992c5a38451d31df4028f5ed3f2078240e
joseph <josephyoongmhk@gmail.com>
2025-11-01 13:11:06 +0000

    Refactor PC module to use output assignment

--- PC.v
+++ PC.v
@@ -1,23 +1,24 @@
 // PC
 // Program Counter (PC)
 
-module PC #(
-) (
+module PC (
     input clk,
     input rst,
     input [31:0] PC_next,
-    output reg [31:0] PC
+    output [31:0] PC
 );
 
 reg [31:0] r_PC;
 
 always @(posedge clk) begin
     if (rst) begin
-        r_PC <= 0;
+        r_PC <= 32'h00000000;
     end
     else begin
-        PC <= PC_next;
+        r_PC <= PC_next;
     end
 end
+
+assign PC = r_PC;
 
 endmodule
